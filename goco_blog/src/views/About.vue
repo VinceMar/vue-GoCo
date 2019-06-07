9c6e16feccff07173e3435a27476d75d8049e2b1
VinceMar <gc_qq744196568@126.com>
2019-06-07 18:00:42 +0800

    生成发布版本

--- goco_blog/src/views/About.vue
+++ goco_blog/src/views/About.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="container">
     <h1>关于本站</h1>
+    <h3>Fork on <a href="https://github.com/VinceMar/vue-GoCo/tree/master/goco_blog">Github</a></h3>
     <ul>
-      <li>HTML5</li>
-      <li>CSS</li>
-      <li>Vue.js</li>
-      <li>vue-cli</li>
-      <li>vue-router</li>
-      <li>axios</li>
+      <li>HTML5+CSS</li>
+      <li>Vue.js+vue-cli</li>
+      <li>vue-router+axios+Vssue</li>
     </ul>
   </div>
 </template>
 
 <style scope>
-  h1 {
+  h1,h3 {
     margin-bottom: 20px;
+    display: inline-block;
   }
   
+  h3 {
+    margin-left: 50px;
+  }
+
   ul {
     font-size: 20px;
   }
